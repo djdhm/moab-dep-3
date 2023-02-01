eae1614cfe50aba3f80c6d91877f097e98cf7a8b
Djamel Dahmane <d.dahmane@criteo.com>
2023-02-01 10:37:57 +0100

    New version without tag
    Change-Id: I9ef417ccbc7c7294194219a1efdea545a2c8e14e

--- lib/tester.go
+++ lib/tester.go
@@ -6,7 +6,7 @@ import (
 	display "github.com/djdhm/external-dep/lib"
 )
 
-const Version = "pseudo-version"
+const Version = "pseudo-version-2"
 
 func TestLib() {
 	fmt.Println("I am moab-dep-3 version " + Version)
